6d9a2496a1d072850b9de9221f86ca45704f4593
Beena Evangelin <graciaevangelin99@gmail.com>
2023-09-25 20:21:25 +0530

    Made a minor refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import NavBar from "./components/NavBar";
 import Home from "./components/Home";
-import Socials from "./components/socials";
+import Socials from "./components/Socials";
 import About from "./components/About";
 import Portfolio from "./components/portfolio";
 import Skills from "./components/skills";
